c16d51497478b77d5446b26f550a43ee1bdcddac
Pranav Nakade <135031313+Pranav1173@users.noreply.github.com>
2023-10-22 15:17:08 +0530

    Update lifo.java

--- TE/SPOS/lifo.java
+++ TE/SPOS/lifo.java
@@ -2,35 +2,28 @@ import java.util.*;
 
 public class lifo {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        // Prompt the user for the number of frames
-        System.out.print("Enter the number of frames: ");
-        int n = sc.nextInt();
-        
-        // Initialize variables
-        int p, i = 0, f = 0, h = 0;
+        // Predefined number of frames
+        int n = 3;
+
+        // Predefined reference string
+        int[] referenceString = {3,1,2,1,5,4,1,3};
+        int p, f = 0, h = 0;
         int[] q = new int[n];
         Stack<Integer> stack = new Stack<>();
-        
-        // Prompt the user for the reference string
-        System.out.println("Enter the reference string (enter -1 to stop):");
-        
-        // Read reference string until the user enters -1
-        while ((p = sc.nextInt()) != -1) {
-            if (!stack.contains(p)) {
+
+        for (int page : referenceString) {
+            if (!stack.contains(page)) {
                 if (stack.size() == n) {
                     int removedPage = stack.pop();
-                    f++;
                 }
-                stack.push(p);
-            } else {
-                stack.removeElement(p);
-                stack.push(p);
+				f++;
+                stack.push(page);
+            } 
+            else {
                 h++;
             }
         }
-        
+
         double hitRatio = (double) h / (h + f);
         System.out.println("Faults: " + f);
         System.out.println("Hits: " + h);
